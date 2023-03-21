93c6993502fc2e6cfae89d3c0c1e7c3fc233325c
Giacomo De Pol <giacomo.depol@hotmail.it>
2023-03-21 19:02:24 +0100

    Remove tour and refresh button

--- setup/src/App.js
+++ setup/src/App.js
@@ -7,6 +7,10 @@ const url = "https://course-api.com/react-tours-project";
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
   const fetchTours = async () => {
     setLoading(true);
     try {
@@ -29,9 +33,21 @@ function App() {
       </main>
     );
   }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
   return (
     <main>
-      <Tours tours />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
